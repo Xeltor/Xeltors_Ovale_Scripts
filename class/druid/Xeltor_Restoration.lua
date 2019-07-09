f4a8662a9265f6a11163eb9882bee64c748856f0
Xeltor <Xeltor@gmail.com>
2019-07-09 17:59:30 +0200

    8.2 restoration

--- class/druid/Xeltor_Restoration.lua
+++ class/druid/Xeltor_Restoration.lua
@@ -3,7 +3,7 @@ local OvaleScripts = __Scripts.OvaleScripts
 
 do
 	local name = "xeltor_restoration_a"
-	local desc = "[Xel][8.1] Druid: Restoration"
+	local desc = "[Xel][8.2] Druid: Restoration"
 	local code = [[
 Include(ovale_common)
 Include(ovale_trinkets_mop)
@@ -14,21 +14,20 @@ Include(druid_common_functions)
 AddIcon specialization=4 help=main
 {
 	# Don't fucking dismount me asshole script.
-	if not Stance(2) and not Stance(3) and not mounted()
+	if not Stance(2) and not Stance(3) and not mounted() and not Dead() and not IsDead()
 	{
 		if HealthPercent() > 0 and HealthPercent() < 70 Spell(renewal)
 		
 		# Ress dead ally
-		if target.HealthPercent() <= 0 and target.IsFriend()
+		if PartyMembersWithHealthPercent(equal 0) >= 1
 		{
-			if InCombat() and not focus.Present() and focus.Exists() and focus.InRange(lifebloom) and target.IsFocus() and Spell(rebirth) and target.InRange(rebirth) and not PreviousGCDSpell(rebirth) Spell(rebirth)
-			if not InCombat() and Spell(revitalize) and not PreviousGCDSpell(revitalize) and { Speed() == 0 or CanMove() > 0 } Spell(revitalize)
+			if not InCombat() and Spell(revitalize usable=1) and not PreviousGCDSpell(revitalize) and { Speed() == 0 or CanMove() > 0 } Spell(revitalize)
 		}
 		
 		if CheckBoxOn(auto) Party_Auto_Target()
 		
 		# Do main rotation.
-		if target.Present() and target.IsFriend() and target.InRange(lifebloom) and target.HealthPercent() < 100
+		if target.Present() and target.IsFriend() and target.InRange(rejuvenation) and target.HealthPercent() < 100
 		{
 			Cooldowns()
 			
@@ -39,15 +38,16 @@ AddIcon specialization=4 help=main
 		if HasFocus()
 		{
 			# Keep Lifebloom on an active tank. Refreshing it with less than 4.5 seconds left in order to proc the final Bloom and not lose any ticks is recommended.
-			if focus.BuffRemains(lifebloom_buff) <= GCD() * 2 Spell(lifebloom)
+			if focus.TicksRemaining(lifebloom_buff) <= 0 and focus.BuffRemains(lifebloom_buff) < 5 and focustarget.Present() and not focustarget.IsFriend() Spell(lifebloom)
 		}
 		
 		# Bored do some dps.
-		if target.IsFriend() and targettarget.Present() and not targettarget.IsFriend() and targettarget.HealthPercent() < 100 and InCombat() and targettarget.InRange(moonfire) TTDPS()
+		if target.IsFriend() and targettarget.Present() and not targettarget.IsFriend() and targettarget.HealthPercent() < 100 and InCombat() and targettarget.InRange(moonfire) and CheckBoxOn(dps) TTDPS()
 		if not target.IsFriend() and target.Present() and target.InRange(moonfire) and InCombat() DPS()
 	}
 }
-AddCheckBox(auto "Party auto target")
+AddCheckBox(auto "Party auto target" default)
+AddCheckBox(dps "Do Deeps")
 
 AddFunction HasFocus
 {
@@ -101,25 +101,25 @@ AddFunction PartyMemberFour
 
 AddFunction Cooldowns 
 {
-	if UnitInRaid() and RaidMembersInRange(wild_growth) >= 6 and RaidMembersWithHealthPercent(less 90) >= 6 and { Speed() == 0 or CanMove() > 0 } or not UnitInRaid() and PartyMembersInRange(wild_growth) >= 4 and PartyMembersWithHealthPercent(less 90) >= 4 and { Speed() == 0 or CanMove() > 0 }
+	if HotCount() >= 8 Spell(flourish)
+	if UnitInRaid() and RaidMembersInRange(wild_growth) >= 6 and RaidMembersWithHealthPercent(less 80) >= 6 and { Speed() == 0 or CanMove() > 0 } or not UnitInRaid() and PartyMembersInRange(wild_growth) >= 3 and PartyMembersWithHealthPercent(less 85) >= 3 and { Speed() == 0 or CanMove() > 0 }
 	{
 		Spell(berserking)
-		Spell(innervate)
+		if ManaPercent() < 80 Spell(innervate)
 		if BuffCountOnAny(wild_growth_buff) == 0 and SpellCooldown(wild_growth) > GCD() Spell(tranquility)
 	}
-	if HotCount() >= 10 Spell(flourish)
 }
 
 AddFunction Rotation
 {
 	# Save dying players immediate.
-	if target.HealthPercent() < 35 HotFix()
+	if target.HealthPercent() < 40 HotFix()
 	# Keep Lifebloom on an active tank. Refreshing it with less than 4.5 seconds left in order to proc the final Bloom and not lose any ticks is recommended.
-	if HasFocus() and focus.BuffRemains(lifebloom_buff) <= 4 Spell(lifebloom)
+	if HasFocus() and focus.TicksRemaining(lifebloom_buff) <= 0 and focus.BuffRemains(lifebloom_buff) < 5 and focustarget.Present() and not focustarget.IsFriend() Spell(lifebloom)
 	# Use Wild Growth when at least 6 members of the raid are damaged and you have some Rejuvenations up.
-	if UnitInRaid() and RaidMembersInRange(wild_growth) >= 6 and RaidMembersWithHealthPercent(less 90) >= 6 and { Speed() == 0 or CanMove() > 0 } Spell(wild_growth)
+	if UnitInRaid() and RaidMembersInRange(wild_growth) >= 6 and RaidMembersWithHealthPercent(less 80) >= 6 and { Speed() == 0 or CanMove() > 0 } Spell(wild_growth)
 	# Use Wild Growth when at least 4 members of the group are damaged.
-	if not UnitInRaid() and PartyMembersInRange(rejuvenation) >= 4 and PartyMembersWithHealthPercent(less 90) >= 3 and { Speed() == 0 or CanMove() > 0 } Spell(wild_growth)
+	if not UnitInRaid() and PartyMembersInRange(wild_growth) >= 3 and PartyMembersWithHealthPercent(less 85) >= 3 and { Speed() == 0 or CanMove() > 0 } Spell(wild_growth)
 	if InCombat() Spell(cenarion_ward)
 	# Keep Rejuvenation on the tank and on members of the group that just took damage or are about to take damage.
 	if not Talent(germination_talent) and target.BuffRemains(rejuvenation_buff) <= 3.5 Spell(rejuvenation)
@@ -127,21 +127,20 @@ AddFunction Rotation
 	# Keep up both Rejuvenations on targets on which the damage is too high for a single one.
 	if Talent(germination_talent) and { Talent(abundance_talent) or target.HealthPercent() < 89 } and target.BuffRemains(germination_buff) <= 3.5 and target.BuffPresent(rejuvenation_buff) and target.BuffRemains(germination_buff) < target.BuffRemains(rejuvenation_buff) Spell(rejuvenation)
 	# Use Swiftmend on a player that just took heavy damage. If they are not in immediate danger, you should apply Rejuvenation to him first.
-	if target.HealthPercent() <= 45 Spell(swiftmend)
 	if target.HealthPercent() <= 80 and { Speed() == 0 or CanMove() > 0 } Spell(regrowth)
 }
 
 AddFunction HotFix
 {
 	if SpellCooldown(swiftmend) > GCD() Spell(ironbark)
+	if SpellCooldown(swiftmend) > GCD() Spell(berserking)
 	Spell(swiftmend)
 }
 
 AddFunction TTDPS
 {
-	if targettarget.DebuffRemains(sunfire_debuff) <= GCD() * 2 and ManaPercent() > 75 Spell(sunfire)
-	if targettarget.DebuffRemains(moonfire_debuff) <= GCD() * 2 and ManaPercent() > 75 Spell(moonfire)
-	if Speed() == 0 or CanMove() > 0 Spell(solar_wrath)
+	if not targettarget.DebuffPresent(sunfire_debuff) and ManaPercent() > 80 Spell(sunfire)
+	if not targettarget.DebuffPresent(moonfire_debuff) and ManaPercent() > 80 Spell(moonfire)
 }
 
 AddFunction DPS
@@ -154,8 +153,8 @@ AddFunction DPS
 	}
 	
 	if Talent(balance_affinity_talent) and not Stance(4) Spell(moonkin_form)
-	if target.DebuffRemains(sunfire_debuff) <= GCD() * 2 Spell(sunfire)
-	if target.DebuffRemains(moonfire_debuff) <= GCD() * 2 Spell(moonfire)
+	if target.DebuffRemains(sunfire_debuff) <= 3 Spell(sunfire)
+	if target.DebuffRemains(moonfire_debuff) <= 3 Spell(moonfire)
 	if Speed() == 0 or CanMove() > 0
 	{
 		if Talent(balance_affinity_talent) Spell(starsurge)
